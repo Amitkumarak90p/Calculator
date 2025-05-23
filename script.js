a68ef99877939e9e3c48d2bd43b235c52753a1d0
Amit Kumar <amitkumarak90p@gmail.com>
2025-05-23 17:33:52 +0530

    all functionality done

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const display = document.querySelector("#display");
+const display = document.querySelector("#display-num");
+const displayop = document.querySelector("#display-op");
+const displayres = document.querySelector("#display-result");
 const clearbtn = document.querySelector("#clear");
 const sevenbtn = document.querySelector("#seven");
 const  onebtn= document.querySelector("#one");
@@ -14,122 +16,209 @@ const plusbtn = document.querySelector("#plus");
 const subtractbtn = document.querySelector("#subtract");
 const multiplybtn = document.querySelector("#multiply");
 const dividebtn = document.querySelector("#divide");
+const equalsbtn = document.querySelector("#equalto");
+let a = "";
+let b = "";
+let operator = "";
+let secondnumber = false;
+let result = 0;
 
 
 document.addEventListener("keypress", (e)=>{
     switch(e.key){
         case "1":
-            console.log("1 was pressed");
             showonscreen(e.key);
+            let a = 1;
             break;
         
         case "2":
-            console.log("2 was pressed");
+            
             showonscreen(e.key);
             break;
         case "3":
-            console.log("3 was pressed");
+            
             showonscreen(e.key);
             break;
         case "4":
-            console.log("4 was pressed");
+            
             showonscreen(e.key);
             break;
         case "5":
-            console.log("5 was pressed");
+            
             showonscreen(e.key);
             break;
         case "6":
-            console.log("6 was pressed");
+            
             showonscreen(e.key);
             break;
         case "7":
-            console.log("7 was pressed");
+            
             showonscreen(e.key);
             break;
         case "8":
-            console.log("8 was pressed");
+            
             showonscreen(e.key);
             break;
         case "9":
-            showonscreen(e.key);
-            console.log("9 was pressed");
+            
             showonscreen(e.key);
             break;
         case "0":
-            console.log("0 was pressed");
+            
             showonscreen(e.key);
             break;
         case "+":
-            console.log("+ was pressed");
-            showonscreen(e.key);
+            plus();
+            break;
+        case "-":
+            minus();
+            break;
+        case "*":
+            multiply();
+            break;
+        case "/":   
+            divide();
+            break;
+        case "Enter":
+            enter();
             break;
     }
 })
 
-sevenbtn.addEventListener("click", ()=>{
-    console.log("7 was pressed");
-    showonscreen("7");
-})
-
-eightbtn.addEventListener("click", ()=>{
-    console.log("8 was pressed");
-    showonscreen("8");
-})
-
-sixbtn.addEventListener("click", ()=>{
-    console.log("6 was pressed");
-    showonscreen("6");
-})
-ninebtn.addEventListener("click", ()=>{
-    console.log("9 was pressed");
-    showonscreen("9");
-})
-fivebtn.addEventListener("click", ()=>{
-    console.log("5 was pressed");
-    showonscreen("5");
-})
-fourbtn.addEventListener("click", ()=>{
-    console.log("4 was pressed");
-    showonscreen("4");
-})
-threebtn.addEventListener("click", ()=>{
-    console.log("3 was pressed");
-    showonscreen("3");
-})
-twobtn.addEventListener("click", ()=>{
-    console.log("2 was pressed");
-    showonscreen("2");
-})
-onebtn.addEventListener("click", ()=>{
-    console.log("1 was pressed");
-    showonscreen("1");
-})
-zerobtn.addEventListener("click", ()=>{
-    console.log("0 was pressed");
-    showonscreen("0");
-})
+sevenbtn.addEventListener("click", () =>showonscreen(7))
+eightbtn.addEventListener("click",  () =>showonscreen(8))
+sixbtn.addEventListener("click",  () => showonscreen(6))
+ninebtn.addEventListener("click",  () => showonscreen(9))
+fivebtn.addEventListener("click",  () => showonscreen(5))
+fourbtn.addEventListener("click",  () => showonscreen(4))
+threebtn.addEventListener("click",  () => showonscreen(3))
+twobtn.addEventListener("click",  () => showonscreen(2))
+onebtn.addEventListener("click",  () => showonscreen(1))
+zerobtn.addEventListener("click",  () => showonscreen(0))
 plusbtn.addEventListener("click", ()=>{
-    console.log("+ was pressed");
-    showonscreen("+");
-});
-subtractbtn.addEventListener("click", ()=>{
-    console.log("- was pressed");
-    showonscreen("-");
+    plus();
 })
-multiplybtn.addEventListener("click", ()=>{
-    console.log("* was pressed");
-    showonscreen("*");
+subtractbtn.addEventListener("click",   () =>{
+    minus();
 })
-dividebtn.addEventListener("click", ()=>{
-    console.log("/ was pressed");
-    showonscreen("/");
+multiplybtn.addEventListener("click",  () =>{
+    multiply();
+})
+dividebtn.addEventListener("click",   () =>{
+    divide();
 })
 clearbtn.addEventListener("click",()=>{
-    display.textContent = "";
-    console.clear();
+ display.textContent = "";
+ displayop.textContent = "";
+ displayres.textContent = "";
+ operator = "";
+    a = "";
+    b = "";
+    result = 0;
+console.clear();
 })
 
+equalsbtn.addEventListener("click", () => {
+    enter();
+});
+
+
+function results(){
+  if (a !== "" && b !== "" && operator === "+") {
+    result = addition(Number(a), Number(b));
+  }
+  if (a !== "" && b !== "" && operator === "-") {
+    result = subtraction(Number(a), Number(b));
+  }
+  if (a !== "" && b !== "" && operator === "*") {
+    result = multiplication(Number(a), Number(b));
+  }
+  if (a !== "" && b !== "" && operator === "/") {
+    result = division(Number(a), Number(b));
+  }
+  console.log("result is "+result);
+  return result;
+}
+function showoperator(value){
+    displayop.textContent = value;
+}
+
 function showonscreen(value){
+    if (secondnumber == false){
+        a += value;
+        console.log("i am a "+a)
+    }
+    else{
+        b+= value;
+        console.log("i am b "+b)
+    }
     display.textContent += value;
+}
+
+function addition(a,b){
+    return a+b;
+}
+function subtraction(a,b){
+    return a-b;
+}
+function multiplication(a,b){
+    return a*b;
+}
+function division(a,b){
+    return a/b;
+}
+
+function plus(){
+    showoperator("Addition");
+    secondnumber = true;
+    display.textContent = "";
+    operator = "+";
+    result += Number(a);
+    a = result;
+    b = "";
+    displayres.textContent = result;
+    console.log("result is "+result);
+}
+function minus(){
+    showoperator("Subtraction")
+    secondnumber = true;
+    display.textContent = "";
+    operator = "-";
+    result += Number(a);
+    a = result;
+    b = "";
+    displayres.textContent = result;
+    console.log("result is "+result);
+}
+function multiply(){
+    showoperator("Multiplication")
+    secondnumber = true;
+    display.textContent = "";
+    operator = "*";
+    result += Number(a);
+    a = result;
+    b = "";
+    displayres.textContent = result;
+    console.log("result is "+result);
+}
+function divide(){
+    showoperator("Division")
+    secondnumber = true;
+    display.textContent = "";
+    operator = "/";
+    result += Number(a);
+    a = result;
+    b = "";
+    displayres.textContent = result;
+    console.log("result is "+result);
+}
+
+function enter(){
+    results();
+    displayres.textContent = result;
+    a = "";
+    b = "";
+    operator = "";
+    secondnumber = false;
+    display.textContent = "";
 }
