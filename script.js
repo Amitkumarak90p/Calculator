01c5fd1a1018648bdd4c7d8f7b6e71e23c6b3bd1
Amit Kumar <amitkumarak90p@gmail.com>
2025-05-23 15:33:11 +0530

    numbers are displayed in the calc display

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const display = document.querySelector("#display");
 const clearbtn = document.querySelector("#clear");
 const sevenbtn = document.querySelector("#seven");
 const  onebtn= document.querySelector("#one");
@@ -19,82 +20,116 @@ document.addEventListener("keypress", (e)=>{
     switch(e.key){
         case "1":
             console.log("1 was pressed");
+            showonscreen(e.key);
             break;
         
         case "2":
             console.log("2 was pressed");
+            showonscreen(e.key);
             break;
         case "3":
             console.log("3 was pressed");
+            showonscreen(e.key);
             break;
         case "4":
             console.log("4 was pressed");
+            showonscreen(e.key);
             break;
         case "5":
             console.log("5 was pressed");
+            showonscreen(e.key);
             break;
         case "6":
             console.log("6 was pressed");
+            showonscreen(e.key);
             break;
         case "7":
             console.log("7 was pressed");
+            showonscreen(e.key);
             break;
         case "8":
             console.log("8 was pressed");
+            showonscreen(e.key);
             break;
         case "9":
+            showonscreen(e.key);
             console.log("9 was pressed");
+            showonscreen(e.key);
             break;
         case "0":
             console.log("0 was pressed");
+            showonscreen(e.key);
             break;
         case "+":
             console.log("+ was pressed");
+            showonscreen(e.key);
             break;
     }
 })
 
 sevenbtn.addEventListener("click", ()=>{
     console.log("7 was pressed");
+    showonscreen("7");
 })
 
 eightbtn.addEventListener("click", ()=>{
     console.log("8 was pressed");
+    showonscreen("8");
 })
 
 sixbtn.addEventListener("click", ()=>{
     console.log("6 was pressed");
+    showonscreen("6");
 })
 ninebtn.addEventListener("click", ()=>{
     console.log("9 was pressed");
+    showonscreen("9");
 })
 fivebtn.addEventListener("click", ()=>{
     console.log("5 was pressed");
+    showonscreen("5");
 })
 fourbtn.addEventListener("click", ()=>{
     console.log("4 was pressed");
+    showonscreen("4");
 })
 threebtn.addEventListener("click", ()=>{
     console.log("3 was pressed");
+    showonscreen("3");
 })
 twobtn.addEventListener("click", ()=>{
     console.log("2 was pressed");
+    showonscreen("2");
 })
 onebtn.addEventListener("click", ()=>{
     console.log("1 was pressed");
+    showonscreen("1");
 })
 zerobtn.addEventListener("click", ()=>{
     console.log("0 was pressed");
+    showonscreen("0");
 })
 plusbtn.addEventListener("click", ()=>{
     console.log("+ was pressed");
+    showonscreen("+");
 });
 subtractbtn.addEventListener("click", ()=>{
     console.log("- was pressed");
+    showonscreen("-");
 })
 multiplybtn.addEventListener("click", ()=>{
     console.log("* was pressed");
+    showonscreen("*");
 })
 dividebtn.addEventListener("click", ()=>{
     console.log("/ was pressed");
+    showonscreen("/");
 })
+clearbtn.addEventListener("click",()=>{
+    display.textContent = "";
+    console.clear();
+})
+
+function showonscreen(value){
+    display.textContent += value;
+}
